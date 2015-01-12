6fd4f613f9241b598f240e49c69b5a27b2721186
arman_arian <arman.hassanpoor2000@gmail.com>
2015-01-12 22:25:27 +0330

    Start!

---
+++ Presentation.MainBoundedContext/Forms/frmLogin.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Presentation.MainBoundedContext.Forms
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
